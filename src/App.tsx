05731efbdbd2211003158a11e5e010aeadf0c14f
mascaradee <lamascaradee@gmail.com>
2025-05-16 18:50:59 +0900

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -6,50 +6,59 @@ import Login from './components/login'
 import CreateAccount from './components/create-account'
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
+import { useEffect, useState } from 'react'
+import LoadingScreen from './components/loadingScreen'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "profile",
+        element: <Profile/>
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount/>
+  }
+])
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout/>,
-      children: [
-        {
-          path: "",
-          element: <Home />
-        },
-        {
-          path: "profile",
-          element: <Profile/>
-        }
-      ]
-      },
-    {
-      path: "/login",
-      element: <Login/>
-    },
-    {
-      path: "/create-account",
-      element: <CreateAccount/>
-    }
-  ])
-
-  const GlobalStyles = createGlobalStyle`
-    ${reset};
-    * {
-      box-sizing: border-box;
-    }
-
-    body {
-      background-color: black;
-      color:white;
-      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    }
-  `;
-
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async () => {
+    setIsLoading(false)
+  }
+  useEffect(() => { 
+    init()
+  }, []);
   return (
     <>
       <GlobalStyles />
-      <RouterProvider router={router} />
+      { isLoading ? <LoadingScreen/> : <RouterProvider router={router} />}
     </>
   )
 }
