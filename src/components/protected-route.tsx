648183fd7adff3805a68e0e21e2ee486cfca0acb
mascaradee <lamascaradee@gmail.com>
2025-05-19 20:16:07 +0900

    로그인 전용화면 및 로그아웃 기능

---
+++ src/components/protected-route.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom"
+import { auth } from "../firebase"
+
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  // 로그인이 필요한 화면 판단
+  const user = auth.currentUser
+  console.log("user: ", user)
+
+  // 비로그인이면 로그인 화면으로
+  if (user === null) {
+    return <Navigate to="/login" />
+  }
+  return children
+}
