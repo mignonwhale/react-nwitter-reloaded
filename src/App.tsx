98c462c74caddb1e45046b6556a01d42ba0d2e9d
mascaradee <lamascaradee@gmail.com>
2025-05-19 19:31:53 +0900

    login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './components/home'
-import Layout from './components/layout'
-import Profile from './components/profile'
-import Login from './components/login'
-import CreateAccount from './components/create-account'
-import { createGlobalStyle } from 'styled-components'
-import reset from 'styled-reset'
-import { useEffect, useState } from 'react'
-import LoadingScreen from './components/loadingScreen'
-import { auth } from './firebase';
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./components/home"
+import Layout from "./components/layout"
+import Profile from "./components/profile"
+import Login from "./components/login"
+import CreateAccount from "./components/create-account"
+import styled, { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
+import { useEffect, useState } from "react"
+import LoadingScreen from "./components/loadingScreen"
+import { auth } from "./firebase"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout/>,
+    element: <Layout />,
     children: [
       {
         path: "",
-        element: <Home />
+        element: <Home />,
       },
       {
         path: "profile",
-        element: <Profile/>
-      }
-    ]
+        element: <Profile />,
+      },
+    ],
   },
   {
     path: "/login",
-    element: <Login/>
+    element: <Login />,
   },
   {
     path: "/create-account",
-    element: <CreateAccount/>
-  }
+    element: <CreateAccount />,
+  },
 ])
 
 const GlobalStyles = createGlobalStyle`
@@ -46,22 +46,28 @@ const GlobalStyles = createGlobalStyle`
     color:white;
     font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
-`;
+`
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true)
   const init = async () => {
-    await auth.authStateReady();
+    await auth.authStateReady()
     setIsLoading(false)
   }
-  useEffect(() => { 
+  useEffect(() => {
     init()
-  }, []);
+  }, [])
   return (
-    <>
+    <Wrapper>
       <GlobalStyles />
-      { isLoading ? <LoadingScreen/> : <RouterProvider router={router} />}
-    </>
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </Wrapper>
   )
 }
 
--- src/components/create-account.tsx
+++ src/components/create-account.tsx
@@ -1,3 +1,84 @@
+import { useState } from "react"
+import styled from "styled-components"
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 420px;
+  padding: 50px 0px;
+`
+const Title = styled.h1`
+  font-size: 42px;
+`
+const Form = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 100%;
+`
+const Input = styled.input`
+  padding: 10px 20px;
+  border-radius: 10px;
+  border: none;
+  width: 100%;
+  font-size: 16px;
+  &[type="submit"] {
+    cursor: pointer;
+    &:hover {
+      opacity: 0.8;
+    }
+  }
+`
+const Error = styled.span`
+  font-weight: 600;
+  color: tomato;
+`
+
 export default function CreateAccount() {
-  return <h1>create account</h1>
+  const [isLoading, setIsLoading] = useState(false) // 서브밋 시 true 서버 응답 후 false
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e
+    if (name === "name") {
+      setName(value)
+    } else if (name === "email") {
+      setEmail(value)
+    } else if (name === "password") {
+      setPassword(value)
+    }
+  }
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    try {
+      console.log(e.target[0].value)
+      console.log(e.target[1].value)
+      console.log(e.target[2].value)
+    } catch (error) {
+      // setError
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  return (
+    <Wrapper>
+      <Title>Log into ❌</Title>
+      <Form onSubmit={onSubmit}>
+        <Input name="name" type="text" value={name} placeholder="name" onChange={onChange} />
+        <Input name="email" type="email" value={email} placeholder="email" onChange={onChange} />
+        <Input name="password" type="password" value={password} placeholder="password" onChange={onChange} />
+        <Input type="submit" value={isLoading ? "Loading..." : "Create Account"} />
+      </Form>
+      {error !== "" ? <Error>{error}</Error> : null}
+    </Wrapper>
+  )
 }
