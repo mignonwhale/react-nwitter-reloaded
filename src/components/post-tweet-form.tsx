12e23633997c216c5c2f183083aff8a09c942dfa
mascaradee <lamascaradee@gmail.com>
2025-05-20 21:15:15 +0900

    image upload
    환경변수 추가

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 import styled from "styled-components"
-import { auth, db } from "../firebase"
-import { addDoc, collection } from "firebase/firestore"
+import { auth, db, storage } from "../firebase"
+import { addDoc, collection, updateDoc } from "firebase/firestore"
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
 
 const Form = styled.form`
   display: flex;
@@ -78,15 +79,32 @@ export default function PostTweetForm() {
       setIsLoading(true)
 
       // 데이터 생성
-      // db : firestore 객체
+      // db : firestore 객체?
       // tweets: collection 명 = 테이블명?
       // {} : 문서 = 데이터
-      await addDoc(collection(db, "tweets"), {
+      const doc = await addDoc(collection(db, "tweets"), {
         tweet,
         createdAt: Date.now(),
         username: user.displayName,
         userId: user.uid,
       })
+
+      // 이미지 파일 생성
+      if (file) {
+        // Storage에 업로드할 경로
+        const locationRef = ref(storage, `/tweets/${user.uid}-${user.displayName}/${doc.id}`)
+
+        const result = await uploadBytes(locationRef, file)
+
+        const url = await getDownloadURL(result.ref)
+
+        await updateDoc(doc, {
+          image: url,
+        })
+      }
+
+      setTweet("")
+      setFile(null)
     } catch (error) {
       console.log(error)
     } finally {
@@ -95,7 +113,7 @@ export default function PostTweetForm() {
   }
   return (
     <Form onSubmit={onsubmit}>
-      <TextArea rows={5} maxLength={180} value={tweet} onChange={onChange} placeholder="What is happening?!" />
+      <TextArea required rows={5} maxLength={180} value={tweet} onChange={onChange} placeholder="What is happening?!" />
       <AttachFileButton htmlFor="file">{file ? "Image added ✅" : "Add Image"}</AttachFileButton>
       <AttachFileInput onChange={onFileChange} id="file" type="file" accept="image/*" />
       <PostButton type="submit" value={isLoading ? "Posting..." : "Post Tweet"} />
